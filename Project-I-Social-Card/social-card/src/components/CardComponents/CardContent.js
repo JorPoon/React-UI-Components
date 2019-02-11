347832da1815ae9f079e53d5731c9163a6e5781c
Jor_Poon <jormpoon@gmail.com>
2019-02-11 16:50:54 -0500

    created card banner and content

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return  (
+        <div>
+            <h3> Get Started With React</h3>
+            <p>React makes it painless to create interactive UIs. Design simple view for each state in your 
+                application.
+            </p>
+            <a href="https://www.reactjs.org">reactjs.org</a>
+        </div>
+    )
+}
+
+export default CardContent;
