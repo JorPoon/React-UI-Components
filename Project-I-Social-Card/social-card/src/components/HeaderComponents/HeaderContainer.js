9fc02a82087455eaa5ed6cc462d06b4f758c9665
Jor_Poon <jormpoon@gmail.com>
2019-02-11 16:11:52 -0500

    added constructor to header container

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './HeaderComponents/ImageThumbnail';
+
+
+const HeaderContainer = () => {
+    return (
+    <div>
+        <ImageThumbnail />
+        <HeaderContent />
+    </div>
+    )
+}
