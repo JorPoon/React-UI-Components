acfeb98a3372ef5b8e69163e85fd9a9e241aa409
Jor_Poon <jormpoon@gmail.com>
2019-02-11 16:15:51 -0500

    added constructor to header content

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContent = () => {
+    return (
+        <div>
+            <h3>Lambda School</h3>
+            <p> Let's Learn React by building a simple interfaces with components. Don't try to
+                overthink it, just keep in simple and have fun. Once you feel comfortable using components 
+                you are well on your way on mastering React!
+            </p>
+        </div>
+    )
+}
