f5ad1e32abb549af5261e1ec8df8e8e1343c00aa
Jor_Poon <jormpoon@gmail.com>
2019-02-12 16:51:33 -0500

    created buttons for calculator in display

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,4 +1,30 @@
 import React from 'react';
 import './Display.css';
+import NumberButton from '../ButtonComponents/NumberButton';
+import ActionButton from '../ButtonComponents/ActionButton';
 
+const CalculatorDisplay = () => {
+    return (
+        <div>
+            <div>Display Box</div>
+            <ActionButton text="clear" />
+            <NumberButton text="/"/>
+            <NumberButton text="7" />
+            <NumberButton text="8" />
+            <NumberButton text="9" />
+            <NumberButton text="*" />
+            <NumberButton text="4" />
+            <NumberButton text="5" />
+            <NumberButton text="6" />
+            <NumberButton text="-" />
+            <NumberButton text="1" />
+            <NumberButton text="2" />
+            <NumberButton text="3" />
+            <NumberButton text="+" />
+            <ActionButton text="0" />
+            <NumberButton text="=" />
+        </div>
+    );
+};
 
+export default CalculatorDisplay;
